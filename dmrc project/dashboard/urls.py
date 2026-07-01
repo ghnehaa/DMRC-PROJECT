885cd23f6b2d7e4703d95144b5c0fcda838faeab
NEHA SHARMA <neha14.december@gmail.com>
2026-07-01 20:03:51 +0530

    Add new simulation update paths to URLs

--- dmrc project/dashboard/urls.py
+++ dmrc project/dashboard/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('layout/', views.layout_view, name='layout'),
     path('export/', views.export_csv,  name='export_csv'),
     path('simulation/tick/', views.simulation_tick, name='simulation_tick'),
+    path('simulation/update_track/', views.update_track, name='update_track'),
+    path('simulation/update_point/', views.update_point, name='update_point'),
 ]
 
